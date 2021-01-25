f1e049797973e66c50608d321cd9a9f0786d6ba6
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-25 08:40:31 -0600

    submitted changes

--- Big-O.js
+++ Big-O.js
@@ -4,4 +4,19 @@ function compressFirstBox(boxes) {
     console.log(boxes[0])
 }
 
-compressFirstBox([1,2,3])
+compressFirstBox([1, 2, 3])
+
+//O(1) - Constant Time
+
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function funChallenge(input) {
+  let a = 10;
+  a = 50 + 3;
+
+  for (let i = 0; i < input.length; i++) {
+    anotherFunction();
+    let stranger = true;
+    a++;
+  }
+  return a;
+}
