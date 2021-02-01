4222826aefb2562d15b92387abee67202df4cdd2
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-01 08:38:13 -0600

    list out data structures and algos

--- Big-O.js
+++ Big-O.js
@@ -1,22 +1,25 @@
 //O(n) - Linear Time
 
-function compressFirstBox(boxes) {
-    console.log(boxes[0])
-}
+// function compressFirstBox(boxes) {
+//     console.log(boxes[0])
+// }
 
-compressFirstBox([1, 2, 3])
+// compressFirstBox([1, 2, 3])
 
-//O(1) - Constant Time
+// //O(1) - Constant Time
 
-// What is the Big O of the below function? (Hint, you may want to go line by line)
-function funChallenge(input) {
-  let a = 10;
-  a = 50 + 3;
+// // What is the Big O of the below function? (Hint, you may want to go line by line)
+// function funChallenge(input) {
+//   let a = 10;
+//   a = 50 + 3;
 
-  for (let i = 0; i < input.length; i++) {
-    anotherFunction();
-    let stranger = true;
-    a++;
-  }
-  return a;
-}
+//   for (let i = 0; i < input.length; i++) {
+//     anotherFunction();
+//     let stranger = true;
+//     a++;
+//   }
+//   return a;
+// }
+
+var a = 1
+
---
+++ DSandALGOS.md
@@ -0,0 +1,18 @@
+Data Structures
+
+    - Arrays
+    - Stacks
+    - Queues
+    - Linked Lists
+    - Trees
+    - Tries
+    - Graphs
+    - Hash Tables
+
+Algorithms
+    - Sorting
+    - Dynamic Programming
+    - BFS + DFS
+    - Recursion
+
+    
