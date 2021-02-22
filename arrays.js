d52f18590c738dc24d37e75e4fccfbd88596ab2c
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-22 06:53:50 -0600

    clear array pag

--- arrays.js
+++ arrays.js
@@ -1,17 +1,1 @@
-const strings = ["a", "b", "c", "d", "e"]
 
-strings.push("e")
-
-console.log(strings)
-
-strings.pop()
-
-console.log(strings)
-
-strings.unshift("1")
-
-console.log(strings)
-
-console.log()
-
-// unshift moves the whole array
