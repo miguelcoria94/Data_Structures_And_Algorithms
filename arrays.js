1af48a1381e1fc591847f9216c7943e1de86f5ea
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-22 07:31:39 -0600

    making a pop method

--- arrays.js
+++ arrays.js
@@ -1,12 +1,30 @@
 class MyArray {
     constructor() {
-        this.lengh = 0
+        this.length = 0
         this.data = {}
     }
 
     get(index) {
         return this.data[index]
     }
+
+    push(item) {
+        this.data[this.length] = item
+        this.length++
+        return this.length
+    }
+
+    pop() {
+        const lastItem = this.data[this.length - 1]
+        
+        delete this.data[this.length - 1]
+        this.length--
+        return lastItem
+    }
 }
 
-const newArr = new MyArray()
+const newArr = new MyArray();
+
+newArr.push("ds")
+newArr.pop()
+console.log(newArr)
