248f0bc0338989898e8c7a88439842cabc00c780
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-22 07:41:42 -0600

    reverse string

--- arrays.js
+++ arrays.js
@@ -21,6 +21,19 @@ class MyArray {
         this.length--
         return lastItem
     }
+
+    delete(index) {
+        const item = this.data[index]
+        this.shiftItems(index)
+    }
+
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++){
+            this.data[i] = this.data[i + 1]
+        }
+        delete this.data[this.length - 1]
+        this.length --
+    }
 }
 
 const newArr = new MyArray();
