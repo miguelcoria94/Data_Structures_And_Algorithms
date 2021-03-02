30fed2f803a91181cf9ee1ffc66246c06c034134
miguelcoria94 <miguelcoria94@gmail.com>
2021-03-02 16:08:18 -0600

    solved maxchars

--- AlgoCasts/exercises/maxchar/index.js
+++ AlgoCasts/exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let stringArray = str.split("")
+    let counter = {}
+    for (let i = 0; i < stringArray.length; i++){
+        if (counter[stringArray[i]] === undefined) {
+            counter[stringArray[i]] = 1
+        } else {
+            counter[stringArray[i]] += 1;
+        }
+    }
+
+    let max = Math.max(...Object.values(counter))
+
+
+
+    return Object.keys(counter).find(key => counter[key] === max)
+}
 
 module.exports = maxChar;
