f0475e9ef7d6d71bcf55e7333defc18da0286460
miguelcoria94 <miguelcoria94@gmail.com>
2021-03-03 21:14:50 -0600

    trying to slove chunks

--- AlgoCasts/exercises/chunk/index.js
+++ AlgoCasts/exercises/chunk/index.js
@@ -8,6 +8,10 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    while () {
+        array.slice(0,size)
+    }
+}
 
 module.exports = chunk;
