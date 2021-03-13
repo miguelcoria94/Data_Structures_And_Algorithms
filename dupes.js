1814134686201e81294f4157c702084497a13e85
miguelcoria94 <miguelcoria94@gmail.com>
2021-03-13 12:32:13 -0600

    set up dupes

---
+++ dupes.js
@@ -0,0 +1,7 @@
+function areThereDuplicates(...args) {
+  console.log(args)
+}
+
+console.log(areThereDuplicates(1, 2, 3))
+console.log(areThereDuplicates(1, 2, 2));
+console.log(areThereDuplicates("a", "b", "c", "a"));
