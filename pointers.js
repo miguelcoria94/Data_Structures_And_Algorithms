bc381a2b7cca3ac5d5aeca90f9b8b59bf2c8749d
miguelcoria94 <miguelcoria94@gmail.com>
2021-03-13 11:44:54 -0600

    setting up problem

---
+++ pointers.js
@@ -0,0 +1,8 @@
+function countUniqueValues(array) {
+    
+}
+
+countUniqueValues([1, 1, 1, 1, 1, 2])
+countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])
+countUniqueValues([])
+countUniqueValues([-2,-1,-1,0,1])
