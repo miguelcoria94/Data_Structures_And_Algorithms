003c50e1d9e3c24fd8a7d53cb4b7c9f693fd1fc2
miguelcoria94 <miguelcoria94@gmail.com>
2021-03-13 12:23:42 -0600

    setting up problem

---
+++ frequencyCounter.js
@@ -0,0 +1,10 @@
+function sameFrequency(num1, num2) {
+    let num1String = num1.toString()
+    let num2String = num2.toString()
+    
+}
+
+console.log(sameFrequency(182, 281))
+console.log(sameFrequency(34,14));
+console.log(sameFrequency(3589578,5879385));
+console.log(sameFrequency(22,222));
